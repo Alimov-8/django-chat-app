6857471f004fc68dcc1706ce8ce515f00c75e1e1
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-08-04 15:34:23 +0500

    login required

--- chats/views.py
+++ chats/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 
 def index(request):
     return render(request, 'chats/index.html')
 
+@login_required
 def room(request, room_name):
     return render(request, 'chats/room.html', {
         'room_name': room_name
