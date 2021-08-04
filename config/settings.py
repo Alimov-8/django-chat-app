aa88f4a1e12ae9622053ddb661ea7ba2754242c0
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-08-04 15:53:41 +0500

    Setting up a channel layer with Redis

--- config/settings.py
+++ config/settings.py
@@ -137,9 +137,20 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 # Redirects
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home' 
 
 
 ASGI_APPLICATION = "config.asgi.application"
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+        'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
