59785e750e4541c4494ee560bb66ec924773d0f6
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-08-05 14:17:01 +0500

    save message into database

--- chats/views.py
+++ chats/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils.safestring import mark_safe
+import json
 
 # Create your views here.
-
 
 def index(request):
     return render(request, 'chats/index.html')
@@ -10,5 +11,9 @@ def index(request):
 @login_required
 def room(request, room_name):
     return render(request, 'chats/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'username': mark_safe(json.dumps(request.user.username)),
     })
+
+
+
